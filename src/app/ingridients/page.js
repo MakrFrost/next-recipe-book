8edee500eb8afea488dc57faccc65f3c12803abe
Mark Kholod <101950285+MakrFrost@users.noreply.github.com>
2023-03-04 19:12:23 +0200

    Add  pages & icons

---
+++ src/app/ingridients/page.js
@@ -0,0 +1,10 @@
+import Head from "./head";
+
+export default function Ingridient({}) {
+  return (
+    <>
+      <Head title={"ShakeShake BAR | Ingridients"} />
+      <div>Ingridients</div>
+    </>
+  );
+}
---
+++ src/app/name/page.js
@@ -0,0 +1,10 @@
+import Head from "./head";
+
+export default function NameCocktail({}) {
+  return (
+    <>
+      <Head title={"Shake-Shake Bar | Cocktail Name"} />
+      <div>Cocktail Name</div>
+    </>
+  );
+}
