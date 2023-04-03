7607ab77213ee4c43df0ee317f81e925b93e1461
Mark Kholod <101950285+MakrFrost@users.noreply.github.com>
2023-04-03 14:11:00 +0300

    Add new components

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 "use client";
-import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Link from "next/link";
 
 import Head from "./head";
+import Cocktail from "./components/cocktail/page";
 import scss from "./page.module.scss";
 
 async function getRandomCocktail() {
@@ -19,7 +19,7 @@ async function getRandomCocktail() {
 }
 
 export default function Homepage() {
-  const [allCocktails, setAllCocktails] = useState([]);
+  // const [allCocktails, setAllCocktails] = useState([]);
   const [random, setRandom] = useState([]);
   // const [load, setLoad] = useState(false);
 
@@ -52,183 +52,17 @@ export default function Homepage() {
             className={scss.main_btn}
             onClick={() => {
               getRandomCocktail()
-                .then(
-                  (data) => (setRandom(...data.drinks), console.log(random))
-                )
+                .then((data) => setRandom(...data.drinks))
                 .catch((error) => console.error(error));
             }}
           >
             Random cocktail
           </button>
-          {random ? (
-            <div className={scss.main_name}>
-              {random.strDrinkThumb ? (
-                <>
-                  <div className={scss.main_wrap}>
-                    <Image
-                      priority
-                      fill
-                      sizes="(max-width: 768px) 100vw,
-              (max-width: 1000px) 50vw,
-              33vw"
-                      src={random.strDrinkThumb}
-                      alt={random.strDrink}
-                    />
-                  </div>
-                  <h1 className={scss.main_drink}>{random.strDrink}</h1>
-                  <p className={scss.main_instruction}>
-                    {random.strInstructions}
-                  </p>
-                  <div className={scss.main_lists}>
-                    <ul className={scss.main_list}>
-                      <li className={scss.main_item}>
-                        <p className={scss.main_list_title}>Ingridients:</p>
-                      </li>
-                      <li className={scss.main_item}>
-                        {random.strIngredient1 ? (
-                          <p className={scss.main_border}>
-                            <b>{random.strIngredient1}</b>
-                          </p>
-                        ) : null}
-                      </li>
-                      <li className={scss.main_item}>
-                        {random.strIngredient2 ? (
-                          <p className={scss.main_border}>
-                            <b>{random.strIngredient2}</b>
-                          </p>
-                        ) : null}
-                      </li>
-                      <li className={scss.main_item}>
-                        {random.strIngredient3 ? (
-                          <p className={scss.main_border}>
-                            <b>{random.strIngredient3}</b>
-                          </p>
-                        ) : null}
-                      </li>
-                      <li className={scss.main_item}>
-                        {random.strIngredient4 ? (
-                          <p className={scss.main_border}>
-                            <b>{random.strIngredient4}</b>
-                          </p>
-                        ) : null}
-                      </li>
-                      <li className={scss.main_item}>
-                        {random.strIngredient5 ? (
-                          <p className={scss.main_border}>
-                            <b>{random.strIngredient5}</b>
-                          </p>
-                        ) : null}
-                      </li>
-                      <li className={scss.main_item}>
-                        {random.strIngredient6 ? (
-                          <p className={scss.main_border}>
-                            <b>{random.strIngredient6}</b>
-                          </p>
-                        ) : null}
-                      </li>
-                      <li className={scss.main_item}>
-                        {random.strIngredient7 ? (
-                          <p className={scss.main_border}>
-                            <b>{random.strIngredient7}</b>
-                          </p>
-                        ) : null}
-                      </li>
-                      <li className={scss.main_item}>
-                        {random.strIngredient8 ? (
-                          <p className={scss.main_border}>
-                            <b>{random.strIngredient8}</b>
-                          </p>
-                        ) : null}
-                      </li>
-                      <li className={scss.main_item}>
-                        {random.strIngredient9 ? (
-                          <p className={scss.main_border}>
-                            <b>{random.strIngredient9}</b>
-                          </p>
-                        ) : null}
-                      </li>
-                      <li className={scss.main_item}>
-                        {random.strIngredient10 ? (
-                          <p className={scss.main_border}>
-                            <b>{random.strIngredient10}</b>
-                          </p>
-                        ) : null}
-                      </li>
-                      <li className={scss.main_item}>
-                        {random.strIngredient11 ? (
-                          <p className={scss.main_border}>
-                            <b>{random.strIngredient11}</b>
-                          </p>
-                        ) : null}
-                      </li>
-                      <li className={scss.main_item}>
-                        {random.strIngredient12 ? (
-                          <p className={scss.main_border}>
-                            <b>{random.strIngredient12}</b>
-                          </p>
-                        ) : null}
-                      </li>
-                      <li className={scss.main_item}>
-                        {random.strIngredient13 ? (
-                          <p className={scss.main_border}>
-                            <b>{random.strIngredient13}</b>
-                          </p>
-                        ) : null}
-                      </li>
-                      <li className={scss.main_item}>
-                        {random.strIngredient14 ? (
-                          <p className={scss.main_border}>
-                            <b>{random.strIngredient14}</b>
-                          </p>
-                        ) : null}
-                      </li>
-                      <li className={scss.main_item}>
-                        {random.strIngredient15 ? (
-                          <p className={scss.main_border}>
-                            <b>{random.strIngredient15}</b>
-                          </p>
-                        ) : null}
-                      </li>
-                    </ul>
-                    <ul className={scss.main_list}>
-                      <li className={scss.main_item}>
-                        <p className={scss.main_list_title}>Measure:</p>
-                      </li>
-                      <li className={scss.main_item}>{random.strMeasure1}</li>
-                      <li className={scss.main_item}>{random.strMeasure2}</li>
-                      <li className={scss.main_item}>{random.strMeasure3}</li>
-                      <li className={scss.main_item}>{random.strMeasure4}</li>
-                      <li className={scss.main_item}>{random.strMeasure5}</li>
-                      <li className={scss.main_item}>{random.strMeasure6}</li>
-                      <li className={scss.main_item}>{random.strMeasure7}</li>
-                      <li className={scss.main_item}>{random.strMeasure8}</li>
-                      <li className={scss.main_item}>{random.strMeasure9}</li>
-                      <li className={scss.main_item}>{random.strMeasure10}</li>
-                      <li className={scss.main_item}>{random.strMeasure11}</li>
-                      <li className={scss.main_item}>{random.strMeasure12}</li>
-                      <li className={scss.main_item}>{random.strMeasure13}</li>
-                      <li className={scss.main_item}>{random.strMeasure14}</li>
-                      <li className={scss.main_item}>{random.strMeasure15}</li>
-                    </ul>
-                  </div>
-                </>
-              ) : null}
-            </div>
-          ) : null}
+
+          {console.log(random)}
+          {random ? <Cocktail cocktail={random} /> : null}
         </div>
       </main>
     </>
   );
-}
-
-//!  заглушка для "нет картинки"
-{
-  /* <Image
-  src={
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
-  }
-  width={400}
-  height={400}
-  alt={random.strDrink}
-/>; */
 }
